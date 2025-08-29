b494525f6a39ad03a0b530afa5893dcf9529d761
DeepMankar-ranium <deep.mankar@ranium.in>
2025-08-29 14:53:08 +0530

    Add migrations for jobs and failed_jobs tables; update welcome email template for role-specific messaging

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -27,7 +27,7 @@
             display: inline-block;
             padding: 10px 20px;
             background-color: #007bff;
-            color: #fff;
+            color: #fff !important;
             text-decoration: none;
             border-radius: 3px;
         }
@@ -39,22 +39,42 @@
 
         <p>Hi {{ $user->name }},</p>
 
-        <p>Thank you for joining NextGig, your new platform for discovering exciting job opportunities and connecting with top employers.</p>
+        @if($user->role->name === 'Job Seeker')
+            <p>Thank you for joining NextGig, your new platform for discovering exciting job opportunities and connecting with top employers.</p>
 
-        <p>To get started, we recommend you:</p>
+            <p>To get started, we recommend you:</p>
 
-        <ul>
-            <li><a href="{{ url('/profile') }}">Complete your profile</a> to attract the best opportunities.</li>
-            <li><a href="{{ url('/Jobs') }}">Start browsing jobs</a> to find your perfect match.</li>
-        </ul>
+            <ul>
+                <li><a href="{{ url('/profile') }}">Complete your profile</a> to attract the best opportunities.</li>
+                <li><a href="{{ url('/jobs') }}">Start browsing jobs</a> to find your perfect match.</li>
+            </ul>
 
-        <p>Our mission is to make the job search process easier and more efficient for everyone. We're excited to have you on board!</p>
+            <div style="text-align: center; margin-top: 30px;">
+                <a href="{{ url('/jobs') }}" class="button">Browse Jobs Now</a>
+            </div>
+
+        @elseif($user->role->name === 'Employer')
+            <p>Thank you for joining NextGig, the best place to find qualified candidates for your open positions.</p>
+
+            <p>To get started, we recommend you:</p>
+
+            <ul>
+                <li><a href="{{ url('/employer/profile') }}">Complete your company profile</a> to attract top talent.</li>
+                <li><a href="{{ url('/jobs/create') }}">Post a new job</a> and start receiving applications.</li>
+            </ul>
+
+            <div style="text-align: center; margin-top: 30px;">
+                <a href="{{ url('/jobs/create') }}" class="button">Post a Job Now</a>
+            </div>
+
+        @else
+             <p>Thank you for joining NextGig!</p>
+        @endif
+
+        <p>We're excited to have you on board!</p>
 
         <p>Best regards,<br>The NextGig Team</p>
 
-        <div style="text-align: center; margin-top: 30px;">
-            <a href="{{ url('/Jobs') }}" class="button">Browse Jobs Now</a>
-        </div>
     </div>
 </body>
 </html>
